9f8adfc58e3c3f204c9299f62648089104c1b7a9
Arpana Sitoula <arp2ctaula@gmail.com>
2022-12-05 20:46:59 +0545

    flags

--- src/Components/ScoreCard.jsx
+++ src/Components/ScoreCard.jsx
@@ -2,28 +2,30 @@ import React, { Component } from 'react'
 
 export default class ScoreCard extends Component {
   render() {
-    var { group, team1,team2,score1,score2 }= this.props
+    var { group, team1,team2,score1,score2,collapse, duration,flag1,flag2}= this.props
     return (
       <div>
       
-    <div className='row justify-content-between m-3 px-3 py-2 bg-dark rounded' data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    <div className={'row justify-content-between m-3 px-3 py-2 bg-dark rounded'} data-bs-toggle="collapse" href={`#${collapse}`} role="button" aria-expanded="false" aria-controls={collapse}>
         <div className='col-2 p-1'>
-          <div className='text-disabled'>{group}</div>  
+          <div className='text-muted'>{group}</div>  
         </div>
         <div className='col-6'>
                 <div className='d-flex justify-content-center'>
-                    <div className='p-1'>{team1}</div>
+                <img className='p-1' style={{height:"30px", width: "50px"}} src={flag1} alt="flag"/>
+                    <div className='p-1'>{team1.slice(0,3)}</div>
                     <div  className='p-1'>{score1}</div>
                     <div className='p-1'>.</div>
                     <div className='p-1'>{score2}</div>
-                    <div className='p-1'>{team2}</div>
+                    <div className='p-1'>{team2.slice(0,3)}</div>
+                <img className='p-1' style={{height:"30px", width: "50px"}} src={flag2} alt="flag"/>
                 </div>
         </div>
        
         <div className='col-2 p-1 d-flex align-content-between cursor-pointer'>
             <div className='row '>
-                <div className='col'>
-                    FT
+                <div className={`col btn btn-warning btn-sm d-${duration}`}>
+                    {duration}
                 </div>
                 <div className='col downArrow '>
                 &rsaquo;
@@ -33,16 +35,12 @@ export default class ScoreCard extends Component {
         
     </div>
    
-    <div class="row justify-content-between px-3 py-2 collapse" id="collapseExample">
-  <div class="card card-body text-dark">
+    <div className="row justify-content-between px-3 py-2 collapse" id={collapse}>
+  <div className="card card-body text-dark">
     Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
   </div>
 </div>
-
       </div>
     )
   }
 }
-//Api Key
-//'X-RapidAPI-Key': '258f47c644mshdae04381cf471bbp1077a9jsne307d2799cac',
-//'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
--- src/Components/Scores.jsx
+++ src/Components/Scores.jsx
@@ -1,13 +1,41 @@
-import React, { Component } from 'react'
-import ScoreCard from './ScoreCard'
+import React, { Component } from "react";
+import ScoreCard from "./ScoreCard";
 
 export default class Scores extends Component {
+ 
   render() {
     return (
-      <div className='container m-3'>
-      <ScoreCard group={"Group C"} team1={"Argentina"} team2={"Australia"} score1={4} score2={0} />
-      <ScoreCard group={"Group F"} team1={"Portugal"} team2={"Switzerland"} score1={2} score2={4}/>
+      <div className="container m-3">
+        <div className="m-3">2022-12-05</div>
+       
+          
+            <div>
+              <ScoreCard
+                group={"Group C"}
+                team1={"Argentina"}
+                team2={"Australia"}
+                score1={2}
+                score2={1}
+                collapse="ex1"
+                duration="FT"
+                flag1={"https://countryflagsapi.com/png/032"}
+                flag2={"https://countryflagsapi.com/png/036"}
+              />
+              <ScoreCard
+                group={"Group F"}
+                team1={"Portugal"}
+                team2={"Switzerland"}
+                score1={2}
+                score2={4}
+                collapse="ex2"
+                duration="none"
+                flag1={"https://countryflagsapi.com/png/620"}
+                flag2={"https://countryflagsapi.com/png/756"}
+              />
+            </div>
+        
+        
       </div>
-    )
+    );
   }
 }
